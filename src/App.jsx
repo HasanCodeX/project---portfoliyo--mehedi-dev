bc2bb1f04d98b8df8a8697058df1047b040fb5b4
hasancodex <dev.hasan10@gmail.com>
2025-03-10 23:17:03 +0600

    -add figma-pdf in src/asset

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import './App.css';
-import Banner from './components/banner.jsx';  // Import Banner component
-import Navbar from './components/navbar.jsx';
-import Footer from './components/footer.jsx';
-import WorkSection from './components/workstation.jsx';
-import About from './components/about.jsx';
-import SkillSection from './components/skill.jsx';
-import ServicesSection from './components/services';
-import ContactSection from './components/contact.jsx';
+import "./App.css";
+import Banner from "./components/banner.jsx"; // Import Banner component
+import Navbar from "./components/navbar.jsx";
+import Footer from "./components/footer.jsx";
+import WorkSection from "./components/workstation.jsx";
+import About from "./components/about.jsx";
+import SkillSection from "./components/skill.jsx";
+import ServicesSection from "./components/services";
+import ContactSection from "./components/contact.jsx";
 import WhatsAppButton from "./components/WhatsAppButton";
-
-
 
 function App() {
   return (
     <div className="App">
-       
-      <Navbar />  {/* Navbar component */}
-      <Banner />  {/* Banner component */}
-      <About/>
-      
-
-  
-<WorkSection/> 
-<ServicesSection/>
-<SkillSection/>
-<ContactSection/>
-<WhatsAppButton />  {/* WhatsAppButton component */}
-<Footer />  {/* Footer component */}
-
+      <Navbar />
+      <WorkSection />
+      <ServicesSection />
+      <SkillSection />
+      <ContactSection />
+      <WhatsAppButton />
+      <Footer />
     </div>
   );
 }
